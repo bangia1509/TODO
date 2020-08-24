d6cc982b718fb3541d54bba836a754e974a25e9d
Ansh Bangia <anshbangia@outlook.com>
2020-08-24 17:17:51 +0530

    nav bar and intray and add button

---
+++ todo_app/lib/models/global.dart
@@ -0,0 +1,9 @@
+import "package:flutter/material.dart";
+
+var darkGreyColor = new Color(0xFF212128);
+var myRed = new Color(0xFFDC4F64);
+var intrayTitle = new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 40.0);
